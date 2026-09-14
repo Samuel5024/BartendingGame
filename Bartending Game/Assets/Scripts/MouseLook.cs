e1169f86c662a92248f2d529182a567fb1056e30
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-09-14 16:43:33 -0700

    Update MouseLook.cs

--- Bartending Game/Assets/Scripts/MouseLook.cs
+++ Bartending Game/Assets/Scripts/MouseLook.cs
@@ -4,12 +4,14 @@ public class MouseLook : MonoBehaviour
 {
 
     public float mouseSensitivity = 300f;
-
+     
     public Transform playerBody;
+    
+    private float xRotation = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -18,6 +20,10 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        playerBody.Rotate(Vector3.up * mouseX);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);  // Limit camera roatation
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);  // Use the mouse X rotation to look left to right
     }
 }
