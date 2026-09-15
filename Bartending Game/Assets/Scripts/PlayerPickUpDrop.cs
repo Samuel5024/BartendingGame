9a9d183d7875841e3d593cd3d944987f139b9c9d
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-09-15 13:22:52 -0700

    Update PlayerPickUpDrop.cs

--- Bartending Game/Assets/Scripts/PlayerPickUpDrop.cs
+++ Bartending Game/Assets/Scripts/PlayerPickUpDrop.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class PlayerPickUpDrop : MonoBehaviour
 {
-    
+    [SerializeField] private Transform playerCameraTransform;
+    [SerializeField] private LayerMask pickUpLayerMask;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            float pickupDistance = 2f;
+            if(Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, pickupDistance, pickUpLayerMask))
+            {
+                Debug.Log(raycastHit.transform);
+            }
+        }
+    }
 }
