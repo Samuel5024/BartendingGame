b2576e75872d0f6faf63b68175b1b89277fd7b96
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-09-21 12:22:12 -0700

    Add drop function to ObjectGrabbable.cs & call it in PlayerPickUpDrop.cs

--- Bartending Game/Assets/Scripts/ObjectGrabbable.cs
+++ Bartending Game/Assets/Scripts/ObjectGrabbable.cs
@@ -15,6 +15,12 @@ public class ObjectGrabbable : MonoBehaviour
         objectRigidbody.useGravity = false;
     }
 
+    public void Drop()
+    {
+        this.objectGrabPointTransform = null;
+        objectRigidbody.useGravity = true;
+    }
+
     private void FixedUpdate()
     {
         if(objectGrabPointTransform != null)
